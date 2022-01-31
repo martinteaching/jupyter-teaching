d7f41d44473247cb0ed038e01f86a4caae782b73
Martin Chapman <contact@martinchapman.co.uk>
2022-01-31 21:22:57 +0000

    Update cleanup script

--- config/cleanup.sh
+++ config/cleanup.sh
@@ -1,2 +1,4 @@
 sudo -H -u jupyter bash -c 'trash-empty'
 sudo -H -u jupyter bash -c 'cat /dev/null > ~/.bash_history && history -c'
+sudo -H -u jupyter bash -c 'rm /home/jupyter/.viminfo*'
+sudo -H -u jupyter bash -c 'rm /home/jupyter/.node_repl_history'
