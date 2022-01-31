c5bed02694c66cfdddf17b6b151a5fc21789dd3e
Martin Chapman <contact@martinchapman.co.uk>
2022-01-31 22:21:25 +0000

    Install Jupyterlab as root

--- setup-1-files.sh
+++ setup-1-files.sh
@@ -1,26 +1,14 @@
 # ---------------------------------------------- #
 export $(cat .env | xargs)
 # ---------------------------------------------- #
-echo "=> setting permissions to disable (some) user edit via file system..."
+echo "=> setting permissions to disable (some) user edits via file system..."
 read -p "=> WARN: setting permissions in this way is experimental. Continue [N|y]? " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     # ---------------------------------------------- #
-    echo "=> setting permissions for jupyter config folder..."
-    ssh -o IdentitiesOnly=yes -i $SSH_KEY $SERVER_USER@$SERVER_ADDRESS "sudo chmod -R ugo-w /home/jupyter/.jupyter"
-    ssh -o IdentitiesOnly=yes -i $SSH_KEY $SERVER_USER@$SERVER_ADDRESS "sudo chmod u+w /home/jupyter/.jupyter"
-    ssh -o IdentitiesOnly=yes -i $SSH_KEY $SERVER_USER@$SERVER_ADDRESS "sudo chmod u+w /home/jupyter/.jupyter/lab"
-    ssh -o IdentitiesOnly=yes -i $SSH_KEY $SERVER_USER@$SERVER_ADDRESS "sudo chmod -R u+w /home/jupyter/.jupyter/lab/workspaces"
-    # ---------------------------------------------- #
-    echo "=> setting permissions for jupyter share folder..."
-    ssh -o IdentitiesOnly=yes -i $SSH_KEY $SERVER_USER@$SERVER_ADDRESS "sudo chmod -R ugo-w /home/jupyter/.local/share/jupyter"
-    ssh -o IdentitiesOnly=yes -i $SSH_KEY $SERVER_USER@$SERVER_ADDRESS "sudo chmod u+w /home/jupyter/.local/share/jupyter"
-    ssh -o IdentitiesOnly=yes -i $SSH_KEY $SERVER_USER@$SERVER_ADDRESS "sudo chmod -R u+w /home/jupyter/.local/share/jupyter/runtime"
-    ssh -o IdentitiesOnly=yes -i $SSH_KEY $SERVER_USER@$SERVER_ADDRESS "sudo chmod u+w /home/jupyter/.local/share/jupyter/notebook_secret"
-    ssh -o IdentitiesOnly=yes -i $SSH_KEY $SERVER_USER@$SERVER_ADDRESS "sudo chmod u+w /home/jupyter/.local/share/jupyter/nbsignatures.db"
-    # ---------------------------------------------- #
-    echo "=> setting permissions for jupyter etc folder..."
-    ssh -o IdentitiesOnly=yes -i $SSH_KEY $SERVER_USER@$SERVER_ADDRESS "sudo chmod -R ugo-w /home/jupyter/.local/etc/jupyter"
+    echo "=> setting general permissions (NB. these can be changed by user, so just a precaution against accidental changes)..."
+    ssh -o IdentitiesOnly=yes -i $SSH_KEY $SERVER_USER@$SERVER_ADDRESS "sudo chmod ugo-w /home/jupyter/.bashrc"
+    ssh -o IdentitiesOnly=yes -i $SSH_KEY $SERVER_USER@$SERVER_ADDRESS "sudo chmod ugo-w /home/jupyter/.profile"
     # ---------------------------------------------- #
 fi
